ba3fd19b9207e6a3a57b4ec0f8937fa673b91e58
WSMorrison <willimo@gmail.com>
2023-03-08 16:10:16 +0000

    Add python version to requirements.txt to hpefully get heroku deployment to work.

--- hardparkers/settings.py
+++ hardparkers/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'hpapp',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
