e513fdf9041fa261e230e48ac83fe083498d10be
WSMorrison <willimo@gmail.com>
2023-03-22 19:10:46 +0000

    Add eventedit.html, EventEdit to views, and the event_edit path to get an event edit page to display, submission not working yet and its still ugly.

--- hpapp/urls.py
+++ hpapp/urls.py
@@ -6,14 +6,16 @@ urlpatterns = [
      path('', views.EventList.as_view(), name='home'),
      path('yourevents/', views.YourEventList.as_view(), name='your_events'),
      path('eventorg/', views.EventOrg.as_view(), name='event_org'),
+     path('eventedit/', views.EventEdit.as_view(), name='event_edit'),
      path('ownconpan/', views.OwnConPan, name='own_con_pan'),
      path('<str:event_name>/', views.EventView.as_view(), name='event_view'),
      path('eventreg/<str:event_name>/',
           views.EventReg.as_view(), name='event_reg'),
      path('eventunreg/<str:event_name>',
           views.EventUnReg.as_view(), name='event_unreg'),
-#     path('eventthanks/<str:event_name>/',
-#          views.EventReg.as_view(), name='event_thanks'),
+#    path('eventthanks/<str:event_name>/',
+#         views.EventReg.as_view(), name='event_thanks'),
      path('register/<str:event_name>/',
           views.UserReg.as_view(), name='user_reg'),
+
 ]
--- hpapp/views.py
+++ hpapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Event, Siteuser, Car
+from .forms import EventForm
 from django import forms
 
 
 # Good
+# Home page.
 class EventList(generic.ListView):
     model = Event
     queryset = Event.objects.order_by('event_date')
@@ -13,6 +15,7 @@ class EventList(generic.ListView):
 
 
 # Good
+# Logged in user's list of events they have already registered for.
 class YourEventList(generic.ListView):
     model = Event
     template_name = 'yourevents.html'
@@ -24,6 +27,7 @@ class YourEventList(generic.ListView):
 
 
 # Good
+# Event organizer's list of events they have organized.
 class EventOrg(generic.ListView):
     model = Event
     template_name = 'eventorg.html'
@@ -35,11 +39,14 @@ class EventOrg(generic.ListView):
 
 
 # Placeholder
+# Owner control panel for approving event organizers, etc.
 def OwnConPan(request):
     return render(request, 'ownconpan.html', {})
 
 
 # Good
+# Logged in user can view an event and either register for it if not
+# already registered, and unregister if already registered.
 class EventView(View):
     def get(self, request, event_name, *args, **kwargs):
         event = get_object_or_404(Event, event_name=event_name)
@@ -61,6 +68,8 @@ class EventView(View):
 
 
 # Good
+# Notification that user has successfully registered for an event,
+# passes them back to their event list with a button.
 class EventReg(View):
     def get(self, request, event_name, *args, **kargs):
         event = get_object_or_404(Event, event_name=event_name)
@@ -69,6 +78,8 @@ class EventReg(View):
 
 
 # Good
+# Notification that a user has successfully left an event,
+# passes them back to their event list with a button.
 class EventUnReg(View):
     def get(self, request, event_name, *args, **kargs):
         event = get_object_or_404(Event, event_name=event_name)
@@ -77,6 +88,8 @@ class EventUnReg(View):
 
 
 # Good
+# The code that adds or removes a user object
+# from the attendee list of an Event.
 class UserReg(View):
     def post(self, request, event_name, *args, **kwargs):
         event = get_object_or_404(Event, event_name=event_name)
@@ -86,3 +99,10 @@ class UserReg(View):
         else:
             event.attendee.add(request.user)
             return render(request, 'eventreg.html', {})
+
+
+# Working
+# Render a form so an event organizer can organize an event.
+class EventEdit(View):
+    def get(self, request):
+        return render(request, 'eventedit.html', {'event_form': EventForm()})
