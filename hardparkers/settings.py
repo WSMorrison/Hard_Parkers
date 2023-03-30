8dbc3421c39946e5e38def23a75bf0a169cef559
WSMorrison <willimo@gmail.com>
2023-03-30 13:45:17 +0000

    Add styling to eventview html.

--- hardparkers/settings.py
+++ hardparkers/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # UNCOMMENT THIS WHEN SETTING DEBUG TO FALSE!
-#X_FRAME_OPTIONS = 'SAMEORIGIN'
+# X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['hardparkers.herokuapp.com', 'localhost']
 
