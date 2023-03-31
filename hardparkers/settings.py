f042c14693fd536e5a660c719337443468bf406c
WSMorrison <willimo@gmail.com>
2023-03-31 19:14:28 +0000

    Herokimage

--- hardparkers/settings.py
+++ hardparkers/settings.py
@@ -147,12 +147,12 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
+MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media'),]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
