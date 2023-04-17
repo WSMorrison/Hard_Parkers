72181c968b93af4ee8b0f6549789f1c32e19639f
WSMorrison <willimo@gmail.com>
2023-04-17 11:11:36 +0000

    Change some copy in forms.

--- hpapp/forms.py
+++ hpapp/forms.py
@@ -16,8 +16,8 @@ def google_maps_ok(value):
 def number_cars_ok(value):
     if value <= 0:
         raise forms.ValidationError("There can't be any parking without cars!")
-    elif value >= 199:
-        raise forms.ValidationError("Don't be ridiculous, Cousin Larry!")
+    elif value > 199:
+        raise forms.ValidationError("Maximum is 199 at this time.")
 
 
 # Form for Event creation.
@@ -25,14 +25,14 @@ class EventForm(forms.ModelForm):
     event_date = forms.DateTimeField(label='The event date:',
                                      widget=forms.TextInput(attrs={
                                        'type': 'date', 'min':
-                                       datetime.now().date()}
-                                       ))
+                                       datetime.now().date()
+                                       }))
     event_date_reg_close = forms.DateTimeField(label='Registration closes:',
                                                widget=forms.TextInput(attrs={
                                                  'type': 'date',
                                                  'min': datetime.now().date(),
-                                                 'max': event_date}
-                                                 ))
+                                                 'max': event_date
+                                                 }))
 
     event_location_url = forms.CharField(label='Share Google Maps link here:',
                                          validators=[google_maps_ok],
@@ -41,7 +41,10 @@ class EventForm(forms.ModelForm):
                                           }))
 
     number_cars = forms.IntegerField(label='How many cars may register?',
-                                     validators=[number_cars_ok])
+                                     validators=[number_cars_ok],
+                                     widget=forms.TextInput(attrs={
+                                      'placeholder': 'Max 199'
+                                      }))
 
     class Meta:
         model = Event
